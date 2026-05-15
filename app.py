60f8e4686bc24bb2bc2c131523dee33df19adf38
msowmi1802-debug <your_email@example.com>
2026-05-15 21:27:34 +0530

    initial commit: todo app with secure login and checkboxes

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev-key-123'
-# Use an absolute path to force the database to be created in your main folder
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+# Using a brand-new database name to ensure a clean slate
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'todo_final_v3.sqlite')
 
 db = SQLAlchemy(app)
 
@@ -21,6 +22,7 @@ class User(db.Model, UserMixin):
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_content = db.Column(db.String(200), nullable=False)
+    is_completed = db.Column(db.Boolean, default=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 # --- LOGIN MANAGER ---
@@ -43,7 +45,7 @@ def index():
 def login():
     if request.method == 'POST':
         user = User.query.filter_by(username=request.form.get('username')).first()
-        if user and user.password == request.form.get('password'):
+        if user and check_password_hash(user.password, request.form.get('password')):
             login_user(user)
             return redirect(url_for('index'))
         flash('Invalid username or password')
@@ -52,7 +54,8 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        new_user = User(username=request.form.get('username'), password=request.form.get('password'))
+        hashed_pw = generate_password_hash(request.form.get('password'))
+        new_user = User(username=request.form.get('username'), password=hashed_pw)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
@@ -65,6 +68,15 @@ def add():
     if content:
         new_task = Todo(task_content=content, user_id=current_user.id)
         db.session.add(new_task)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/toggle/<int:task_id>', methods=['POST'])
+@login_required
+def toggle_task(task_id):
+    task = Todo.query.get_or_404(task_id)
+    if task.user_id == current_user.id:
+        task.is_completed = not task.is_completed
         db.session.commit()
     return redirect(url_for('index'))
 
